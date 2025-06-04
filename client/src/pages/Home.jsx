4323ed8e7d63ef14976e00479a58af29769b23c8
abdul8840 <abdul14941naseer@gmail.com>
2025-06-04 12:08:33 +0530

    add meta tag

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,10 +6,17 @@ import FAQ from '../components/HomeContent/FAQ'
 import HomeWebContent from '../components/HomeContent/HomeWebContent'
 import HomeWebBanner from '../components/HomeContent/HomeWebBanner'
 import CallToAction from '../components/Others/CallToAction'
+import { Helmet } from 'react-helmet';
 
 const Home = () => {
   return (
     <div>
+      <Helmet>
+        <meta name="google-site-verification" content="zOo5mDIS73dG0bMDz6uZq6aFIhd7uGRph2WRoWa13sQ" />
+        <title>Your Page Title</title>
+        <meta name="description" content="Your page description" />
+      </Helmet>
+
       <HomeHero />
       <HomeBanner />
       <HomeWebBanner />
